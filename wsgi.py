880f2cca1b287c2a9b5abcf884052a57a01b30c7
peter--bolier--zero <bolier@xs4all.nl>
2017-06-23 14:27:13 +0200

    wsgi expect application so modded wrapper

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from service.adviceapi import app
+# Warpper for uwsgi. 
+from service.adviceapi import app as application
+import os
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    application.run(host='0.0.0.0', port = port)
